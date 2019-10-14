b195fa5c94210f22bb83885c0c53d5d9e4b353df
Angela Liu <angelayh.liu@gmail.com>
2019-10-14 12:45:25 -0400

    Input lab01

---
+++ lab01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+user_input = input("May I have your name: ")
+date_input = input("What date of today: ")
+print("Hello, ",user_input," ! Happy ",date_input," of the week!")
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+my_list =["192.168.0.1",5060,"UP"]
+print("The first item is the list (IP): "+ my_list[0])
+print("The second item is the list (IP): " + mu_list[1])
+print("The third item is the list (IP): " + my_list[2])
+
