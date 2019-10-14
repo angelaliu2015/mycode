e4985cbe999895a66e9ba8abc7db36ca58ccb069
Angela Liu <angelayh.liu@gmail.com>
2019-10-14 13:35:29 -0400

    working on list

--- lab01.py
+++ lab01.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 user_input = input("May I have your name: ")
-date_input = input("What date of today: ")
-print("Hello, ",user_input," ! Happy ",date_input," of the week!")
-
+day_input = input("What date of today: ")
+print("Hello, ",user_input,"! Happy ",day_input," !")
+print(f'Hello, {user_input}! Happy {day_input}!')
--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 my_list =["192.168.0.1",5060,"UP"]
 print("The first item is the list (IP): "+ my_list[0])
-print("The second item is the list (IP): " + mu_list[1])
+print("The second item is the list (IP): " + str(my_list[1]))
 print("The third item is the list (IP): " + my_list[2])
-
+new_list = [5060, "80", 55, "10.0.0.1","10.20.30.1","ssh"]
+print(f"When I {new_list[-1]} into IP address {new_list[-2]} or {new_list[-3]}. I am unable to ping ports str({new_list[0]}), str({new_list[1]}), or str({new_list[2]}).")
